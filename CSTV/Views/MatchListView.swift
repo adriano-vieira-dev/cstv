8063893b0f5ded90797139f641df2324a9db1e10
Adriano Vieira <adrianoj@icloud.com>
2024-08-30 00:09:26 -0300

    Splash Screen

---
+++ CSTV/Views/MatchListView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchListView.swift
+//  CSTV
+//
+//  Created by Adriano Vieira on 30/08/24.
+//
+
+import SwiftUI
+
+struct MatchListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MatchListView()
+}
---
+++ CSTV/Views/SplashScreenView.swift
@@ -0,0 +1,48 @@
+//
+//  SplashScreenView.swift
+//  CSTV
+//
+//  Created by Adriano Vieira on 30/08/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var showSplash = true
+    
+    // Given time to keep the fuze logo on screen
+    let SPLASH_SCREEN_TIME = 2
+    
+    var splash: some View {
+        Image(.fuze)
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .center
+            )
+            .background(Color.background)
+    }
+    
+    var body: some View {
+        if showSplash {
+            splash
+                .onAppear {
+                    onAppear()
+                }
+        } else {
+            MatchListView()
+        }
+    }
+    
+    func onAppear() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(SPLASH_SCREEN_TIME)) {
+            withAnimation {
+                showSplash = false
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
