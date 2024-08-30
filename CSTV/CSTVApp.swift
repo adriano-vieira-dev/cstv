1d0584dbdf28293f34ae55c70cd12b13b8b80e06
Adriano Vieira <adrianoj@icloud.com>
2024-08-29 23:16:01 -0300

    Initial Commit

---
+++ CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Adriano Vieira on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
